cd1366f9ee9f7e57262d42d969065843b9eaee40
Max Horn <max@quendi.de>
2019-06-30 21:26:05 +0200

    Spelling / grammar / punctuation

--- gap/base/methsel.gd
+++ gap/base/methsel.gd
@@ -21,30 +21,27 @@ DeclareGlobalFunction( "CallMethods" );
 
 # Possible return values for recognition methods:
 #
-# method successfully computed a homomorphism
-# (used to be 'true')
+# The method successfully computed a homomorphism (used to be 'true').
 #BindGlobal("Success", MakeImmutable("Success"));
 BindGlobal("Success", true);    # HACK: use old value for now, to ease transition
 
-# method is never applicable to this kind of group (e.g. input is
-# non-solvable, but method is only applicable to solvable groups),
-# don't bother to try it again
-# (used to be 'false')
+# The method is never applicable to this kind of group (e.g. input is
+# non-solvable, but method is only applicable to solvable groups; or method
+# only applies to permutation groups, but input is a matrix group). so don't
+# bother to try it again (used to be 'false').
 #BindGlobal("NeverApplicable", MakeImmutable("NeverApplicable"));
 BindGlobal("NeverApplicable", false);    # HACK: use old value for now, to ease transition
 
-# The method temporarily failed, that it could be sensible to call it
-# again in this situation at a later stage. This value is typical for a Las
-# Vegas algorithm using randomised methods, which has failed, but which may
-# succeed when called again.
-# (used to be 'fail')
+# The method temporarily failed, but it could be sensible to call it again in
+# this situation at a later stage. This value is typical for a Las Vegas
+# algorithm using randomised methods, which has failed, but which may succeed
+# when called again (used to be 'fail').
 #BindGlobal("TemporaryFailure", MakeImmutable("TemporaryFailure"));
 BindGlobal("TemporaryFailure", fail);    # HACK: use old value for now, to ease transition
 
-# method needs more information (e.g. things like whether group is
-# solvable; transitive; etc.) -> try again later if new information
-# becomes available
-# (used to be 'NotApplicable')
+# The method needs more information (e.g. things like whether group is
+# solvable; transitive; etc.) -> try again later if new information becomes
+# available (used to be 'NotApplicable').
 BindGlobal("NotEnoughInformation", "NotEnoughInformation");
 
 
