4bc778c862004ab91dd10ff13cab392bdcfc485f
Sergio Siccha <siccha@mathematik.uni-kl.de>
2022-02-24 16:21:03 +0100

    Mandarin docs

--- gap/base/recognition.gd
+++ gap/base/recognition.gd
@@ -474,6 +474,16 @@ BindGlobal( "SLPforElementFuncsGeneric", rec() );
 # Refer to overview paper by Baarnhielm, Holt, Charles, Eamonn, sections "5.2
 # The main algorithm" and "5.4 Crisis management".
 # Explain safe and unsafe nodes.
+# Explain in which situations a crisis can be evoked:
+# - only in a leaf: mandarin can't be written as SLPs in nice gens, this does
+#   not happen in non-leaf nodes
+# - mandarins can't be mapped under reduction homomorphism
+# - 
+# Note that when implementing verification via presentations, that can also
+# evoke crises. Then I'd make sense to remove the "MANDARIN_" prefix from the
+# name.
+# TODO Wait a second, if the generators of the root can be expressed as SLPs in
+# the nice gens, then the kernels must be correct, right?!
 BindGlobal("MANDARIN_CRISIS", MakeImmutable("MANDARIN_CRISIS"));
 BindGlobal("NUM_MANDARINS_DEFAULT_VALUE", 100);
 DeclareFilter( "IsSafeForMandarins" );
