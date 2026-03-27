gap-packages/recog

commit 1cd94c0fb8404762229a0054b9ecda467b1b3352

Minor code cleanup in classical.gi (#360)

gap/matrix/classical.gi
@@ -755,7 +755,7 @@ function(recognise, grp)
         fi;
      fi;
 
-     if recognise.needKF  = true  then
+     if recognise.needKroneckerFactors  = true  then
             if recognise.bc = "unknown" then
                 recognise.needBaseChange := true;
             else
@@ -768,14 +768,14 @@ function(recognise, grp)
       fi;
 
       if recognise.needPlusMinus = true then
-            if recognise.kf = "unknown" then
-                recognise.needKF := true;
+            if recognise.kroneckerFactors = "unknown" then
+                recognise.needKroneckerFactors := true;
                 return fail;
             fi;
-            if recognise.kf = false then
+            if recognise.kroneckerFactors = false then
                 return fail;
             fi;
-            kf := recognise.kf;
+            kf := recognise.kroneckerFactors;
             o1 := ProjectiveOrder( kf[1] )[1];
             o2 := ProjectiveOrder( kf[2] )[1];
             #o1 := Order( kf[1] );
@@ -820,13 +820,13 @@ function(recognise, grp)
         fi;
 
         if recognise.needDecompose = true then
-            if recognise.kf = "unknown" then
-                recognise.needKF := true;
+            if recognise.kroneckerFactors = "unknown" then
+                recognise.needKroneckerFactors := true;
                 return fail;
-            elif recognise.kf = false then
+            elif recognise.kroneckerFactors = false then
                 return fail;
             fi;
-            kf := recognise.kf;
+            kf := recognise.kroneckerFactors;
 
             if not kf[1] in Group(recognise.sq1) then
                 AddSet(recognise.sq1,kf[1]);
@@ -1453,10 +1453,10 @@ function(recognise, grp)
         if not 4 in recognise.LB then
             return fail;
         fi;
-        if not 2 in  recognise.LS then
+        if not 2 in recognise.LS then
             return fail;
         fi;
-    elif d = 4  and q >= 7 and IsPowerOfTwo(q+1) then
+    elif d = 4 and q >= 7 and IsPowerOfTwo(q+1) then
         if not 4 in recognise.LB then
             return fail;
         fi;
@@ -2337,7 +2337,7 @@ function( arg )
                    # LB = e's of large basic ppd elements
                    # E2 = e's of ppd elements for e=d/2
                    E := [], LE := [], BE := [], LB := [],
-                   LS := [], E2 := [], LE2 := [], BE2 := [],
+                   LS := [], E2 := [],
                    g := fail,
                    cpol := fail,
                    isppd := fail,
@@ -2358,7 +2358,7 @@ function( arg )
                    porders := [],
                    hasSpecialEle := false,
                    bc := "unknown",
-                   kf := "unknown",
+                   kroneckerFactors := "unknown",
                    plusminus := Set([]),
                    sq1 := [Z(q)*One(GL(2,q))],
                    sq2 := [Z(q)*One(GL(2,q))],
@@ -2368,7 +2368,7 @@ function( arg )
                    needOrders := false,
                    needPOrders := false,
                    needBaseChange := false,
-                   needKF := false,
+                   needKroneckerFactors := false,
                    needPlusMinus := false,
                    needDecompose := false,
                    needLB := false,

commit 346add1ac6968510d8cb023c3f8c9bb8db9c518a

Fix regressions in RecogniseClassical
One was introduced in PR #360 and broke naming of SO(+1,4,8).

One was introduced in PR #386 and broke naming of SO(+1,8,2).

Also add code to avoid annoying warnings being printed when
naming SO(+1,4,4).

gap/matrix/classical.gi
@@ -92,15 +92,15 @@ HasLBGgt5 := function( m, p, a, e )
         ## Now we know (e+1) divides ppds and (e+1) has to be
         ## a prime since all ppds are at least (e+1)
         if not IsPrimeInt (e+1) then
-             return false;
-         fi;
-         # if (e+1)^2 does not divide m, then not large
-         if not (m mod (e+1)^2) = 0 then
-             return false;
-         fi;
-     fi;
+            return false;
+        fi;
+        # if (e+1)^2 does not divide m, then not large
+        if not (m mod (e+1)^2) = 0 then
+            return false;
+        fi;
+    fi;
 
-     return true;
+    return true;
 end;
 
 
@@ -824,7 +824,7 @@ function(recognise)
                 if kf = false then
                     kf := KroneckerFactors( (g^2)^recognise.bc );
                 fi;
-                recognise.kf  := kf;
+                recognise.kroneckerFactors := kf;
             fi;
       fi;
 
@@ -1776,7 +1776,7 @@ end);
 BindRecogMethod(FindHomMethodsClassical, "NonGenericOrthogonalPlus",
 "tests whether group is non-generic O+",
 function(recognise)
-    local grp, d, q, gp1, gp2, CheckFlag, pgrp, sc, isHypForm, ol;
+    local grp, d, q, gp1, gp2, CheckFlag, pgrp, sc, isHypForm, ol, ord;
     grp := recognise.grp;
 
     isHypForm := f -> IsSesquilinearForm(f) and IsHyperbolicForm(f);
@@ -1845,7 +1845,7 @@ function(recognise)
         # Both the conformal orthogonal and the omega for d = 8 and q = 2 have
         # orbits of these lengths. The maximal subgroups of the conformal
         # orthogonal don't have these orbit lengths.
-        if OrbitLengthsDomain(pgrp) <> [ 120, 135 ] then
+        if SortedList(OrbitLengthsDomain(pgrp)) <> [ 120, 135 ] then
            recognise.isOmegaContained := false;
            return NeverApplicable;
         fi;
@@ -1961,15 +1961,23 @@ function(recognise)
            Size(gp2/recognise.scalars) mod 12 = 0 then
                 return CheckFlag();
         fi;
-    elif d = 4 and q =  4 then
+    elif d = 4 and q = 4 then
         # the conformal group can have orbits of length 75 and 180
         # the group Omega can have orbits of lengths 75 and 60
         ol := Length(Orbit(grp, IdentityMat(d, GF(q))[1]));
         if not ol in [60,75,180] then
             return NeverApplicable;
         fi;
-        if RECOG.EstimateProjOrder(grp) mod 3600 <> 0 then
-             recognise.isOmegaContained := false;
+
+        # avoid warnings 'Giving up, Schreier tree is not shallow' (in general
+        # one shouldn't ignore this kind of warning but here we know which
+        # groups are input, and it is acceptable)
+        ol := InfoLevel(InfoOrb);
+        SetInfoLevel(InfoOrb, 0);
+        ord := RECOG.EstimateProjOrder(grp);
+        SetInfoLevel(InfoOrb, ol);
+        if ord mod 3600 <> 0 then  # FIXME: Giving up, Schreier tree is not shallow
+            recognise.isOmegaContained := false;
              return NeverApplicable;
         fi;
         if recognise.needDecompose = false then
@@ -1982,7 +1990,7 @@ function(recognise)
              Size(gp1/recognise.scalars), "x", Size(gp2/recognise.scalars));
         if Size(gp1/recognise.scalars) mod 3 = 0 and
            Size(gp2/recognise.scalars) mod 3 = 0 then
-                return CheckFlag();
+            return CheckFlag();
         fi;
     elif d = 4 and q = 5 then
         ## Added fast test 4.7.2019 ACN
@@ -2024,7 +2032,7 @@ function(recognise)
              Size(gp1/recognise.scalars), "x", Size(gp2/recognise.scalars));
         if Size(gp1/recognise.scalars) mod 168 = 0 and
            Size(gp2/recognise.scalars) mod 168 = 0 then
-                return CheckFlag();
+            return CheckFlag();
         fi;
     elif d = 4 and q = 9 then
         ## Added fast test 4.7.2019 ACN
@@ -2142,7 +2150,7 @@ function(recognise)
 # ACN May 2007
             if Comm(h,g) <> One(grp) then
                 if Comm(Comm(h,g),g) <> One(grp) then
-                        return CheckFlag();
+                    return CheckFlag();
                 fi;
             fi;
         od;
@@ -2253,7 +2261,7 @@ function(recognise)
                return CheckFlag();
             fi;
         else
-               return CheckFlag();
+           return CheckFlag();
         fi;
         recognise.isOmegaContained := false;
         return NeverApplicable;